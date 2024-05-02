c1ab622ac343ce859b8547272a5f0a23bc3b55c7
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-02 23:51:00 +0530

    auto push

---
+++ env-prod/banckend.tfvars
@@ -0,0 +1,3 @@
+bucket   = "terraform-statefile-saurabh"
+key      = "dev/terraform-vpc/teraform.tfstate"
+region   = "us-east-1"
--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
 module "tf-module" {
-  source = "./vendor/modules/vpc"
+  source              = "./vendor/modules/vpc"
+
+  VPC_CIDR            = var.VPC_CIDR
+  PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
 }
