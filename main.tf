8abe85b474074321bfa4bdecf7a2d86d079cca7c
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-03 15:02:20 +0530

    auto push

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "tf-module" {
-  source = "./vendor/modules/vpc"
+  source              = "./vendor/modules/vpc"
 
   VPC_CIDR            = var.VPC_CIDR
   PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
   PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
-  ENV      = var.ENV
-  ZONE  = var.ZONE
+  ENV                 = var.ENV
+  ZONE                = var.ZONE
 }
