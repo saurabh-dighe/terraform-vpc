68a96dfdd699f05a64d764fdc81c94798411fcd6
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-04 01:01:58 +0530

    auto push

--- main.tf
+++ main.tf
@@ -6,4 +6,5 @@ module "tf-module" {
   PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
   ENV                 = var.ENV
   ZONE                = var.ZONE
+  DEFAULT_VPC_CIDR    = var.DEFAULT_VPC_CIDR
 }
